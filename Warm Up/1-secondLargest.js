9478a387f899e062cd9a3e7e1abe273099cafc91
Vinayak Vishwanath Gosain <vinayakvvg@Vinayaks-Laptop.local>
2025-06-07 22:48:27 +0530

    Warm up dsa questions pushed

---
+++ Warm Up/1-secondLargest.js
@@ -0,0 +1,34 @@
+// Write a function secondLargest(arr) that takes an array of numbers and returns the second largest unique number in the array.
+
+// Requirements
+// The array must contain at least two numbers.
+// If the array contains all equal elements or only one unique element, return: "No second largest found"
+// If the array has less than two elements, return: "Array should have at least two numbers"
+
+const findSecondLargest = function (arr) {
+  if (arr.length < 2) {
+    return "Array should have at least two numbers";
+  }
+
+  let largest = -Infinity,
+    secondLargest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] !== largest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest === -Infinity
+    ? "No second largest found"
+    : secondLargest;
+};
+
+console.log(findSecondLargest([2, -6, 4, 8, 1, 7, 8, -9]));
+console.log(findSecondLargest([4, 4, 4, 4]));
+console.log(findSecondLargest([2]));
+console.log(findSecondLargest([10, 20]));
+
+// checks for -ve and +ve numbers
+// checks for duplicate elements in the array and returns not duplicate value
---
+++ Warm Up/2-reverseInteger.js
@@ -0,0 +1,30 @@
+// Question: Reverse Integer with Overflow Check
+// Write a function reverse(x) that takes a 32-bit signed integer and returns its digits reversed. If the reversed value overflows the 32-bit signed integer range, return 0.
+
+// Requirements
+// Reverse the digits of a 32-bit signed integer.
+// Return 0 if the result overflows.
+
+const reverseInteger = function (num) {
+  const copyNum = num;
+  num = Math.abs(num);
+  let rev = 0;
+
+  while (num != 0) {
+    const rem = num % 10;
+    rev = rev * 10 + rem;
+    num = Math.floor(num / 10);
+  }
+
+  const limit = Math.pow(2, 31); // or 2**31
+  if (rev > limit - 1) {
+    // checking only positive value as we did Math.abs above
+    return "0 (overflow)";
+  }
+
+  return copyNum < 0 ? -rev : rev;
+};
+
+console.log(reverseInteger(123));
+console.log(reverseInteger(-123));
+console.log(reverseInteger(1534236469));
